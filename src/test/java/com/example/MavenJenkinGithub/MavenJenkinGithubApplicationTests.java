5dc7785e880e3da39438946037031dc42d1a03ab
seetashende <ashubshende002@gmail.com>
2023-03-09 10:11:08 +0530

    first_commit

---
+++ src/test/java/com/example/MavenJenkinGithub/MavenJenkinGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MavenJenkinGithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenJenkinGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
